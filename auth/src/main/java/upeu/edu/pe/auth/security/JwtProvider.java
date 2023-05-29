29cb1924576899e7e0e6a1e27ad5ef480bed19bd
IVANMAMANI2003 <ivanyomm.2003@gmail.com>
2023-05-29 08:16:09 -0500

    .

--- auth/src/main/java/upeu/edu/pe/auth/security/JwtProvider.java
+++ auth/src/main/java/upeu/edu/pe/auth/security/JwtProvider.java
@@ -5,12 +5,13 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import jakarta.annotation.PostConstruct;
 import upeu.edu.pe.auth.entity.AuthUser;
 
 @Component
--- auth/src/main/java/upeu/edu/pe/auth/security/SecurityConfig.java
+++ auth/src/main/java/upeu/edu/pe/auth/security/SecurityConfig.java
@@ -1,23 +1,13 @@
 package upeu.edu.pe.auth.security;
 
-import static org.springframework.security.config.Customizer.*;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests((authz) -> authz
-                .anyRequest().authenticated()
-            )
-            .httpBasic(withDefaults());
-        return http.build();
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().anyRequest().permitAll();
     }
-
 }
