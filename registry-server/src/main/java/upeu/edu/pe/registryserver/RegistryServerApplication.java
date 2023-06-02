6e990916cbacb22d0fc2267616c7318d2f4ad3cd
IVANMAMANI2003 <ivanyomm.2003@gmail.com>
2023-06-02 08:46:15 -0500

    .

--- registry-server/src/main/java/upeu/edu/pe/registryserver/RegistryServerApplication.java
+++ registry-server/src/main/java/upeu/edu/pe/registryserver/RegistryServerApplication.java
@@ -2,9 +2,10 @@ package upeu.edu.pe.registryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-
+@EnableEurekaServer
 public class RegistryServerApplication {
 
 	public static void main(String[] args) {
