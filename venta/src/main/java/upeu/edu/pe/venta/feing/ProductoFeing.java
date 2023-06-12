69bcab2533cc17be1fa2ef55691bb4e1ea2c38ac
IVANMAMANI2003 <ivanyomm.2003@gmail.com>
2023-06-12 12:02:41 -0500

    .

--- venta/src/main/java/upeu/edu/pe/venta/feing/ProductoFeing.java
+++ venta/src/main/java/upeu/edu/pe/venta/feing/ProductoFeing.java
@@ -4,11 +4,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import upeu.edu.pe.venta.dto.Producto;
 
-@FeignClient(name = "catalogo", path = "/producto")
+@FeignClient(name = "catalogo")
+@RequestMapping(value = "/producto")
 public interface ProductoFeing {
 
     @GetMapping(value = "/{id}")
